65dbdd696e2260e4f162b0d777392a3274a2847d
ismaeelakin <ismaeelakin@gmail.com>
2025-01-07 15:44:05 +0100

    assignments.js

---
+++ assignments.js
@@ -0,0 +1,10 @@
+let countryName = 'Nigeria';
+let continentName = 'Africa';
+let population = '200000000';
+console.log(countryName);
+console.log(continentName);
+console.log(population);
+let isIsland = 'Badagry'
+let language = ''
+console.log(isIsland);
+console.log(language);
