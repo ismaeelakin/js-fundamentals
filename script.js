d9f66443aebc50d648c344d1a91ada89fcbbc892
ismaeelakin <ismaeelakin@gmail.com>
2025-01-08 12:57:07 +0100

    operators and precedence

--- script.js
+++ script.js
@@ -3,30 +3,62 @@
 //if (js === 'boring') alert('javascript is FUN!');
 //console.log(42+20+8-10);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+//let javascriptIsFun = true;
+//console.log(javascriptIsFun);
 
 //console.log(typeof true);
-console.log(typeof javascriptIsFun);
+//console.log(typeof javascriptIsFun);
 //console.log(typeof 'jonas');
 //console.log(typeof 23);
 
-javascriptIsFun = 'Yes!';
-console.log(typeof javascriptIsFun);
+//javascriptIsFun = 'Yes!';
+//console.log(typeof javascriptIsFun);
 
 
-let year;
-console.log(year);
-console.log(typeof year);
-year = 1991
-console.log(year);
-console.log(typeof null);
-let age = 30;
-age =31;
+//let year;
+//console.log(year);
+//console.log(typeof year);
+//year = 1991
+//console.log(year);
+//console.log(typeof null);
+//let age = 30;
+//age =31;
 //const birthYear = 1991;
 //birthYear = 1990;
-var job = "programmer"
-job = 'teacher'
-lastName = 'schedtmann';
-console.log(lastName);
+//var job = "programmer"
+//job = 'teacher'
+//lastName = 'schedtmann';
+//console.log(lastName);
+//math operators
+//const now = 2037;
+//const ageJohn = now - 1991;
+//const ageSarah = now - 2018;
+//console.log(ageJohn, ageSarah);
+
+//console.log(ageJohn * 2, ageSarah / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3
+//const firstName = 'jonas';
+//const lastName = 'schmedmann';
+//console.log(firstName + ' ' + lastName);
+//assignment operators
+//let x = 10 + 5; // 15
+//console.log(x);
+//x += 10; // x = x + 10 = 25
+//x *= 4; // x = x * 4 = 100
+//x ++; // x = x + 1 = 101
+//x --; // x = x - 1 = 99
+//x --;
+//console.log(x);
+//comparison operators
+const now = 2037;
+const ageJohn = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x,y);
+ const averageAge = (ageJohn + ageSarah) /2
+ console.log(ageJohn, ageSarah, averageAge);
+ 
 
