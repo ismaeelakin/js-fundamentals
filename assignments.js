b0599b84b9d8bc358c9c789ddcec3f12e4947aed
ismaeelakin <ismaeelakin@gmail.com>
2025-01-08 16:04:30 +0100

    code challege update for assignment.js

--- assignments.js
+++ assignments.js
@@ -16,6 +16,6 @@ const heightMark = 1.69;
 const massJohn = 92;
 const heightJohn = 1.95;
 
-const BMIMark = massMark / heightJohn ** 2;
+const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
