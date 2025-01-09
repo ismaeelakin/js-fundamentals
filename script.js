20b4beff26b63b6271ea4a4e667ad1053f2ab7ba
ismaeelakin <ismaeelakin@gmail.com>
2025-01-09 16:47:21 +0100

    type coercion

--- script.js
+++ script.js
@@ -102,7 +102,8 @@ console.log(`string
 
 // console.log(century); // Print the century to the console
 
-// Type conversion and coersion
+// Type conversion and coercion
+// type conversion
 const inputYear = '1991';
 console.log(Number (inputYear), inputYear);
 console.log(Number (inputYear) + 18);
@@ -110,4 +111,11 @@ console.log(Number (inputYear) + 18);
 console.log(Number ('jonas'));
 console.log(typeof NaN);
 console.log(String (23), 23);
+
+//type coercion
+console.log(' I am ' + 23 + 'years old');
+console.log('23' - '10' - '3');
+console.log('23' + '10' + '3');
+console.log(23 + 10 + 3);
+console.log('23' * '2');
 
